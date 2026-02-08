7527895509c55eee798c3cef4eaa7f9d4702cfa8
Jessica Hamilton <jessica@jesikat.nz>
2026-02-08 15:55:27 +1300

    Test client of `rust-mpd`
    A very simple usage of `rust-mpd` that fails when `mpd` is run on Windows.

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate mpd;
+
+use mpd::Client;
+use mpd::search::{Query, Term, Window};
+
+fn main() {
+    let mut connection = Client::connect("172.18.176.1:6601").unwrap();
+
+	let mut query = Query::new();
+	let query = query.and(Term::Any, "");
+	let (mut index, chunk_size) = (0, 100);
+	let mut files: Vec<String> = vec![];
+	loop {
+		let search = connection.search(query, Window::from((index, index+chunk_size)));
+		println!("{:?}", search);
+
+		match search {
+			Ok(_) => (),
+			Err(e) => {
+				println!("{:?}", e);
+				println!("Read a total of {} items", index);
+				break
+			}
+		}
+
+		println!("Read {} items..., up to {}", chunk_size, index);
+
+		index += chunk_size;
+	}
+}
